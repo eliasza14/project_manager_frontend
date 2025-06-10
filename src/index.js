52ba79fbaae81b4c84751713fecea468c21ab17e
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2025-06-10 10:55:59 +0300

    message

--- src/index.js
+++ src/index.js
@@ -1,24 +1,61 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// // PrimeReact core styles
+// import 'primereact/resources/themes/lara-light-blue/theme.css'; // or another theme
+// import 'primereact/resources/primereact.min.css';
+// import 'primeicons/primeicons.css';
+
+// // Optional: PrimeFlex if you use utility classes
+// import 'primeflex/primeflex.css';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-// PrimeReact core styles
+
+
 import 'primereact/resources/themes/lara-light-blue/theme.css'; // or another theme
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
 // Optional: PrimeFlex if you use utility classes
 import 'primeflex/primeflex.css';
+import App from './App';
+// import "bulma/css/bulma.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// import 'primereact/resources/themes/lara-light-indigo/theme.css';   // theme
+// import 'primereact/resources/primereact.css';   
+import axios from "axios"
+
+axios.defaults.withCredentials=true;
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
